6fd1bb8cd2c542819170656961b9969219c350fd
Aurélien Vasseur <aurelienvasseur60@gmail.com>
2024-03-13 09:52:05 +0100

    Authentication status management improved in Header

--- src/components/Header/actions.ts
+++ src/components/Header/actions.ts
@@ -1,10 +1,8 @@
 "use server";
 
-import { redirect } from "next/navigation";
 import { createClient } from "@/lib/supabase/server";
 
 export async function signOut() {
   const supabase = createClient();
   await supabase.auth.signOut();
-  redirect("/");
 }
